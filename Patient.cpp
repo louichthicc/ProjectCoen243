1e19e2bdf45a8b24ecc6f5a18f4f3603397d9920
louichthicc <yyf2709@gmail.com>
2024-11-22 17:10:55 -0500

    Patient is done, except for Print_Patient_info() in Patient.cpp

--- Patient.cpp
+++ Patient.cpp
@@ -5,3 +5,85 @@
 //
 
 #include "Patient.h"
+
+Patient::Patient() {}
+Patient::Patient(std::string name,std::string LastName,int ID, std::string DOB, std::string BT, std::string D, std::string DOA, std::string DD) {
+    patientFirstName = name;
+    patientLastName = LastName;
+    patientId = ID;
+    dateOfBirth = DOB;
+    diagnosis = D;
+    dateOfAdmission = DOA;
+    dischargeDate = DD;
+
+}
+
+
+//Getters and Setters
+std::string Patient::getFirstName() {
+    return patientFirstName;
+}
+void Patient::setFirstName(std::string Name) {
+    patientFirstName = Name;
+}
+std::string Patient::getLastName() {
+    return patientLastName;
+}
+void Patient::setLastName(std::string Name) {
+    patientLastName = Name;
+}
+int Patient::getId() {
+    return patientId;
+}
+void Patient::setId(int Id) {
+    patientId = Id;
+}
+std::string Patient::getDateOfBirth() {
+    return dateOfBirth;
+}
+void Patient::setDateOfBirth(std::string Date) {
+    dateOfBirth = Date;
+}
+std::string Patient::getBloodType() {
+    return bloodType;
+}
+void Patient::setBloodType(std::string BloodType) {
+    bloodType = BloodType;
+}
+std::string Patient::getDiagnosis() {
+    return diagnosis;
+}
+void Patient::setDiagnosis(std::string Diagnosis) {
+    diagnosis = Diagnosis;
+}
+std::string Patient::getDateOfAdmission() {
+    return dateOfAdmission;
+}
+void Patient::setDateOfAdmission(std::string DateOfAdmission) {
+    dateOfAdmission = DateOfAdmission;
+}
+std::string Patient::getDischargeDate() {
+    return dischargeDate;
+}
+void Patient::setDischargeDate(std::string DischargeDate) {
+    dischargeDate = DischargeDate;
+}
+
+//other functions
+bool Patient::isDischarged() {
+    if (dischargeDate == "-1") {
+        return true;
+    }
+        return false;
+}
+std::string Patient::Patient_status() {
+    if (diagnosis.find("critical")) {
+        return "Critical";
+    }
+     if(diagnosis.find("moderate")) {
+         return "Moderate";
+     }
+    return "Stable";
+}
+void Patient::Print_Patient_Info() {
+}
--- Patient.h
+++ Patient.h
@@ -6,11 +6,47 @@
 
 #ifndef PATIENT_H
 #define PATIENT_H
-
+#include <string>
 
 
 class Patient {
+public:
+    Patient();
+    Patient(std::string name,std::string LastName,int ID, std::string DOB, std::string BT, std::string D, std::string DOA, std::string DD);
 
+
+    //Getters and Setters
+    std::string getFirstName();
+    void setFirstName(std::string Name);
+    std::string getLastName();
+    void setLastName(std::string Name);
+    int getId();
+    void setId(int Id);
+    std::string getDateOfBirth();
+    void setDateOfBirth(std::string Date);
+    std::string getBloodType();
+    void setBloodType(std::string BloodType);
+    std::string getDiagnosis();
+    void setDiagnosis(std::string Diagnosis);
+    std::string getDateOfAdmission();
+    void setDateOfAdmission(std::string DateOfAdmission);
+    std::string getDischargeDate();
+    void setDischargeDate(std::string DischargeDate);
+
+    //other functions
+    bool isDischarged();
+    std::string Patient_status();
+    void Print_Patient_Info();
+
+private:
+    std::string patientFirstName;
+    std::string patientLastName;
+    int patientId;
+    std::string dateOfBirth;
+    std:: string bloodType;
+    std::string diagnosis;
+    std::string dateOfAdmission;
+    std::string dischargeDate;
 };
 
 
