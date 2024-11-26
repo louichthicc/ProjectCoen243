f753685765d13ca58005a28e648a4f5b122ca372
louichthicc <yyf2709@gmail.com>
2024-11-26 13:41:46 -0500

    Doctor is done, except for Print_Doctor_info() in Doctor.cpp

--- Doctor.cpp
+++ Doctor.cpp
@@ -5,3 +5,62 @@
 //
 
 #include "Doctor.h"
+Doctor::Doctor(){}
+Doctor::Doctor(std::string name, std::string lastName, int ID, std::string S, int XP, double salary, double B) {
+    doctorFirstName = name;
+    doctorLastName = lastName;
+    doctorID = ID;
+    speciality = S;
+    yearsOfExperience = XP;
+    baseSalary = salary;
+    bonus = B;
+}
+//getters and setters
+std::string Doctor::getFirstName() {
+    return doctorFirstName;
+}
+std::string Doctor::getLastName() {
+    return doctorLastName;
+}
+int Doctor::getID() {
+    return doctorID;
+}
+std::string Doctor::getSpeciality() {
+    return speciality;
+}
+int Doctor::getYearsOfExperience() {
+    return yearsOfExperience;
+}
+double Doctor::getSalary() {
+    return baseSalary;
+}
+double Doctor::getBonus() {
+    return bonus;
+}
+
+void Doctor::setFirstName(std::string name) {
+    doctorFirstName = name;
+}
+void Doctor::setLastName(std::string lastName) {
+    doctorLastName = lastName;
+}
+void Doctor::setID(int ID) {
+    doctorID = ID;
+}
+void Doctor::setSpeciality(std::string S) {
+    speciality = S;
+}
+void Doctor::setYearsOfExperience(int years) {
+    yearsOfExperience = years;
+}
+void Doctor::setSalary(double salary) {
+    baseSalary = salary;
+}
+void Doctor::setBonus(double B) {
+    bonus = B;
+}
+
+double Doctor::Calculate_Salary() {
+    return baseSalary * (1+bonus);
+}
+void Doctor::Print_Doctor_Info(){}
--- Doctor.h
+++ Doctor.h
@@ -6,11 +6,41 @@
 
 #ifndef DOCTOR_H
 #define DOCTOR_H
-
+#include <string>
 
 
 class Doctor {
+public:
+    Doctor();
+    Doctor(std::string name, std::string lastName, int ID, std::string speciality, int XP, double salary, double bonus);
+//getters and setters
+    std::string getFirstName();
+    std::string getLastName();
+    int getID();
+    std::string getSpeciality();
+    int getYearsOfExperience();
+    double getSalary();
+    double getBonus();
 
+    void setFirstName(std::string name);
+    void setLastName(std::string lastName);
+    void setID(int ID);
+    void setSpeciality(std::string speciality);
+    void setYearsOfExperience(int years);
+    void setSalary(double salary);
+    void setBonus(double bonus);
+
+    double Calculate_Salary();
+    void Print_Doctor_Info();
+
+private:
+    std::string doctorFirstName;
+    std::string doctorLastName;
+    int doctorID;
+    std::string speciality;
+    int yearsOfExperience;
+    double baseSalary;
+    double bonus;
 };
 
 
